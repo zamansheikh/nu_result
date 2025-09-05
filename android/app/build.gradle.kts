b896ae75d41cb3d8e8b44c494aedb1cde40e685c
Zaman Sheikh <zaman6545@gmail.com>
2025-09-05 20:34:34 +0600

    fix: Correct versionCode in build.gradle.kts from 10 to 8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 10
+        versionCode = 8
         versionName = flutter.versionName
     }
 
