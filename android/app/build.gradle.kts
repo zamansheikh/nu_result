0e13f89b207157fdf7e2c65a9da1a0a76164e177
Zaman Sheikh <zaman6545@gmail.com>
2025-09-05 17:44:05 +0600

    fix: Update versionCode to a static value of 10 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = 10
         versionName = flutter.versionName
     }
 
